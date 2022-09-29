411a4ef915daf3ec50273bed56a393da789c6efc
Logan Hodgins <loganhodginsofficial@gmail.com>
2022-09-29 00:01:52 -0500

    Added better spinner

--- client/src/components/Board/LoadingGlass.js
+++ client/src/components/Board/LoadingGlass.js
@@ -2,7 +2,7 @@ import classes from './LoadingGlass.module.css';
 
 const LoadingGlass = () => {
   return <div className={classes['loading-glass']}>
-    <div className={classes['dot-falling']}></div>
+    <div className={classes.loader}></div>
   </div>
 };
 
